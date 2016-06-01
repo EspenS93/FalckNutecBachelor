ff62356f4e6d3a9c454a6c62f86b6463d05698aa
RemoteUser <RemoteUser@WIN-QT7KGL9HG25>
2016-06-02 00:05:55 +0200

    La til nødvendig info for å sende mail

---
+++ FalckNutecBachelor/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+
+namespace FalckNutecBachelor
+{
+    public class SendMail
+    {
+        WebMail.Send(to: customerEmail,
+            subject: "Help request from - " + customerName,
+            body: customerRequest
+    }
+}
